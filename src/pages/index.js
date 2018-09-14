e96e9e7d95eaeb5fcb1d584e8eb5247f8db0bf92
wildpow <element1028@gmail.com>
2018-09-13 18:07:26 -0700

    testing out Netlfiy forms in gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+    <li><Link to="/contact/">Basic contact form</Link></li>
+
+  </div>
+)
+
+export default IndexPage
